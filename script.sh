60b2cdc6f2289f290176f08850da74d9b4ac73fc
YHONATAN-LEONARDO <yonconortorez@gmail.com>
2026-05-04 09:32:43 -0400

    Initial commit: retos web del 1 al 10

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+echo "Construyendo y levantando contenedores..."
+docker-compose up --build -d
+echo "Retos disponibles en puertos 8001 a 8010"
+echo "Ejecuta 'docker-compose down' para detener"
